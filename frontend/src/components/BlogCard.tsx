410175d01f9bf07104414a9864149c905f3d88ef
sudhanshuk1404 <singhsudhanshu7656@gmail.com>
2024-07-13 21:08:17 +0530

    cardchanges

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -25,6 +25,9 @@ export const BlogCard = ({
           <div className="flex justify-center flex-col pl-2 flex justify-center flex-col">
             <Circle />
           </div>
+          <div className="pl-2 font-thin text-slate-400 text-sm flex justify-center flex-col">
+            {publishedDate}
+          </div>
         </div>
         <div className="text-xl font-semibold pt-2">{title}</div>
         <div className="text-md font-thin">{content.slice(0, 100) + "..."}</div>
